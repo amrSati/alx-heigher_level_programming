b4fb15da3f90eee3fb381be619062ac8e092a038
Amro Sati <amrosatti98@gmail.com>
2024-06-28 12:16:35 +0300

    task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Script that lists all `State` objects from the database `hbtn_0e_6_usa`
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    """main function so that the code doesn't run when imported
+    """
+    if len(argv) != 4:
+        print("usage: {} <username> <password> <database>".format(argv[0]))
+        exit(1)
+
+    engine = create_engine(
+                    'mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                    format(argv[1], argv[2], argv[3])
+                )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.add(State(name='Louisiana'))
+
+    state = session.query(State).filter(State.name == ('Louisiana')).first()
+    print(state.id)
+
+    session.commit()
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
