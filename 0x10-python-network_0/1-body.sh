6fbf1876dc24af2f744a916c3e136b74fa2ea142
Amro Sati <amrosatti98@gmail.com>
2024-07-06 23:22:25 +0300

    fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays the body of the response of a GET request
-if [ "$(curl -o /dev/null -s -w "%{http_code}\n" "$1")" -eq "200" ]; then curl "$1"; fi
+if [ "$(curl -o /dev/null -s -w "%{http_code}\n" "$1")" -eq "200" ]; then curl -L "$1"; fi
