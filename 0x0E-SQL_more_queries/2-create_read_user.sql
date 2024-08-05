6d96eaa5d06b7356a3d78fdd1350d1e10ca6fafa
Amro Sati <amrosatti98@gmail.com>
2024-08-05 07:55:53 +0300

    task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,9 +1,9 @@
 -- Creates a database and a read user on that database
 
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
-CREATE USER IF NOT EXISTS `user_0d_2`@`localhost`
-	IDENTIFIED BY `user_0d_2_pwd`;
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+	IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
-	ON `hbtn_0d_2`.*
-	TO `user_0d_2`@`localhost`;
+	ON 'hbtn_0d_2'.*
+	TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
