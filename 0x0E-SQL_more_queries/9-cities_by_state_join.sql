debba23387d1796a4adabe7c1b88bb20f739fb9c
Amro Sati <amrosatti98@gmail.com>
2024-08-05 10:44:09 +0300

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,7 @@
 -- Lists all cities in the database
 
 SELECT cities.id, cities.name, states.name
-FROM states
-	JOIN cities;
+FROM cities
+	JOIN states
+WHERE cities.state_id = states.id
+ORDER BY cities.id ASC;
