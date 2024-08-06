3d3442c2b7377fa2b5bc62bef50458f2b4807e24
Amro Sati <amrosatti98@gmail.com>
2024-08-06 18:21:40 +0300

    task 102

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- Lists all shows by their rating
+
+SELECT title, SUM(rate) AS rating
+FROM tv_shows AS shows
+	INNER JOIN tv_show_ratings AS ratings
+		ON shows.id = ratings.show_id
+GROUP BY title
+ORDER BY rating DESC;
